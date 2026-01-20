81ef1317dd6cbe1d83131c662fe645c34ed95146
Mehboob12-creat <mehboob.mugul@gmail.com>
2026-01-20 21:06:50 +0500

    Create str-ctr.js

---
+++ pages/str-ctr.js
@@ -0,0 +1,11 @@
+import PlaceholderPage from "../components/PlaceholderPage";
+
+export default function StrCtrPage() {
+  return (
+    <PlaceholderPage
+      title="STR / CTR Recommendations"
+      subtitle="System-generated recommendations with reasons."
+      note="This module will present STR and CTR recommendations based on risk indicators. Recommendations are advisory only and require compliance officer review before any action."
+    />
+  );
+}

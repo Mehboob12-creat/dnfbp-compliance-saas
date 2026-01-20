18aa5baf923a2839c994642e6d97042b99ba6fb1
Mehboob12-creat <mehboob.mugul@gmail.com>
2026-01-20 21:05:22 +0500

    Create inspection-data.js

---
+++ pages/inspection-data.js
@@ -0,0 +1,11 @@
+import PlaceholderPage from "../components/PlaceholderPage";
+
+export default function InspectionDataPage() {
+  return (
+    <PlaceholderPage
+      title="Inspection Data"
+      subtitle="Inspection ledger exports and documentation bundles."
+      note="This module will allow generation of inspection registers, document ZIP files, and printable reports for regulatory review. All outputs are export-only and do not communicate with regulators."
+    />
+  );
+}

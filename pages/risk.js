1bbb45b2c0d38a4a63ccf6845cf767c9ad02ef84
Mehboob12-creat <mehboob.mugul@gmail.com>
2026-01-20 21:06:09 +0500

    Create risk.js

---
+++ pages/risk.js
@@ -0,0 +1,11 @@
+import PlaceholderPage from "../components/PlaceholderPage";
+
+export default function RiskPage() {
+  return (
+    <PlaceholderPage
+      title="Risk Assessment"
+      subtitle="Automated risk rating with explainable reasons."
+      note="Risk ratings are calculated using a rules-based methodology aligned with AML/CFT regulations. Reports generated here support compliance documentation and internal decision-making."
+    />
+  );
+}

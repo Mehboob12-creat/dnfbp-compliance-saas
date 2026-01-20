149048c5e5dd94354a2ea032eb08067d04dfb201
Mehboob12-creat <mehboob.mugul@gmail.com>
2026-01-20 21:07:27 +0500

    Create controls.js

---
+++ pages/controls.js
@@ -0,0 +1,11 @@
+import PlaceholderPage from "../components/PlaceholderPage";
+
+export default function ControlsPage() {
+  return (
+    <PlaceholderPage
+      title="Internal Controls"
+      subtitle="Governance, training, and audit documentation."
+      note="This area will store compliance officer nominations, audit reports, and training evidence to support AML/CFT governance and inspections."
+    />
+  );
+}
